bf66282524a2c876d9ff28c607fbd230c636451a
0alenka069-cyber <0alenka069@gmail.com>
2026-01-31 00:05:58 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+import { renderProducts, renderCart, updateCartCounter, loadProductPage, toggleCart, initLogoSpin } from "./ui.js";
+import { clearCart } from "./cart.js";
+import { checkout } from "./checkout.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  renderProducts();
+  renderCart();
+  updateCartCounter();
+  loadProductPage();
+  initLogoSpin();
+
+  document.getElementById("cartToggle")?.addEventListener("click", toggleCart);
+
+  document.getElementById("clearCartBtn")?.addEventListener("click", () => {
+    clearCart();
+    renderCart();
+    updateCartCounter();
+  });
+
+  document.getElementById("checkoutBtn")?.addEventListener("click", checkout);
+
+  document.getElementById("cartOverlay")?.addEventListener("click", toggleCart);
+
+  document.addEventListener("keydown", e => {
+    if (e.key === "Escape") {
+      toggleCart();
+    }
+  });
+
+});
